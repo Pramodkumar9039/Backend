4a169a214f49942d66526ad5cc15a9b6686f1f38
Pramodkumar9039 <pramodbhande9039@gmail.com>
2026-06-07 20:48:36 +0530

    connectDB to Server

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// require("dotenv").config();
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config();
+
+connectDB();
+
+/*import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+
+import express from "express";
+const app = express();
+
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error" , (err) => {
+            console.log("Error in connecting to database" , err);
+            throw err;
+        })
+
+        app.listen(process.env.PORT , () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    }catch(err){
+        console.log("Error in connecting to database" , err);
+        throw err;
+    }
+})
+    */
